eb9fa717e7c050aa3200d8d3d6790f1f4ca6f065
Yassine Zakhama <yzakhama@gmail.com>
2025-11-15 13:05:47 +0100

    add and use lombok

---
+++ server/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.7"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+}
--- server/src/main/java/com/hsgg/config/AppProperties.java
+++ server/src/main/java/com/hsgg/config/AppProperties.java
@@ -1,20 +1,16 @@
 package com.hsgg.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Configuration;
 
+@Setter
+@Getter
 @Configuration
 @ConfigurationProperties(prefix = "app")
 @ConfigurationPropertiesScan
 public class AppProperties {
     private String learningMaterialsDir;
-
-    public String getLearningMaterialsDir() {
-        return learningMaterialsDir;
-    }
-
-    public void setLearningMaterialsDir(String learningMaterialsDir) {
-        this.learningMaterialsDir = learningMaterialsDir;
-    }
 }
