9a3270611040eabcebde3d7531bc667782960333
Yassine Zakhama <yzakhama@gmail.com>
2025-11-15 13:57:20 +0100

    fix client path

--- server/src/main/java/com/hsgg/config/WebConfig.java
+++ server/src/main/java/com/hsgg/config/WebConfig.java
@@ -1,15 +1,20 @@
 package com.hsgg.config;
 
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.nio.file.Path;
+
 @Configuration
+@ConfigurationPropertiesScan
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        Path clientPath = Path.of("").toAbsolutePath().resolve("../client");
         registry.addResourceHandler("/**")
-                .addResourceLocations("file:../client/");
+                .addResourceLocations("file:" + clientPath + "/");
     }
 }
