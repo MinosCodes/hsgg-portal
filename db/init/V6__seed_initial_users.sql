ae2e67908d0409adb7fff61e18f5d431163dd7f3
Yassine Zakhama <yassine.zakhama@alumni.fh-aachen.de>
2025-11-19 00:21:47 +0100

    fix users credentials

--- db/init/V6__seed_initial_users.sql
+++ db/init/V6__seed_initial_users.sql
@@ -1,4 +1,4 @@
 INSERT INTO users (first_name, last_name, username, password_hash, role)
-VALUES ('System', 'Admin', 'admin', '$2a$10$6./B9e8k2t9uEJuY9Vc3hu0JYyheVQwWI4sJ0bbN4uAqN2oW9TQW6', 'ADMIN'),
-       ('Default', 'Teacher', 'teacher', '$2a$10$z2rmP0v8wCkfrFyLBlntrODm7WvZnfcHykjc1sYk4A3zETgkthMby', 'TEACHER'),
-       ('Default', 'Student', 'student', '$2a$10$88o9hMphT1TcWz2sWWkqQu1K1oF8LxzBm2i2dR9akZWCjkjU3Gv1K', 'STUDENT');
+VALUES ('System', 'Admin', 'admin', '$2a$10$KUUK9LUJo3G4o9KS5bNSdudutqxYPsJM8YKyjkzmAl5CPZRKiPUja', 'ADMIN'),
+       ('Default', 'Teacher', 'teacher', '$2a$10$MaqpehMOa61TsH15Ig3grui7syUuNW1LmpfCseupw.2xMXABYztg.', 'TEACHER'),
+       ('Default', 'Student', 'student', '$2a$10$S1V/vK4EnRNtxOSuU0QrLeGcq//P6hKxr6wgxwcpW7va10jrYt4Hy', 'STUDENT');
