2e21965571198630dd879b6f4df767da7061a4f9
Yassine Zakhama <yzakhama@gmail.com>
2025-11-18 21:55:13 +0100

    move auth logic to service

--- server/src/main/java/com/hsgg/security/auth/AuthController.java
+++ server/src/main/java/com/hsgg/security/auth/AuthController.java
@@ -1,14 +1,8 @@
 package com.hsgg.security.auth;
 
-import com.hsgg.security.auth.dtos.AuthResponseDto;
 import com.hsgg.security.auth.dtos.LoginRequestDto;
 import com.hsgg.security.auth.dtos.RegisterRequestDto;
-import com.hsgg.security.jwt.JwtService;
-import com.hsgg.security.user.User;
-import com.hsgg.security.user.UserRepository;
-import com.hsgg.security.user.UserRole;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,50 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-	private final UserRepository userRepository;
-	private final PasswordEncoder passwordEncoder;
-	private final JwtService jwtService;
+	private final AuthService authService;
 
-	public AuthController(UserRepository userRepository,
-						  PasswordEncoder passwordEncoder,
-						  JwtService jwtService) {
-		this.userRepository = userRepository;
-		this.passwordEncoder = passwordEncoder;
-		this.jwtService = jwtService;
+	public AuthController(AuthService authService) {
+		this.authService = authService;
 	}
 
 	@PostMapping("/register")
 	public ResponseEntity<?> register(@RequestBody RegisterRequestDto request) {
-		System.out.println(request.username());
-		System.out.println(request.password());
-		if (userRepository.existsByUsername(request.username())) {
-			return ResponseEntity.badRequest().body("Username already taken.");
-		}
-
-		User user = new User();
-		user.setUsername(request.username());
-		user.setPassword(passwordEncoder.encode(request.password()));
-		user.setRole(UserRole.STUDENT);
-		user.setFirstName(request.firstname());
-		user.setLastName(request.lastname());
-
-		userRepository.save(user);
-
-		return ResponseEntity.ok("Registered successfully");
+		return this.authService.register(request);
 	}
 
 	@PostMapping("/login")
 	public ResponseEntity<?> login(@RequestBody LoginRequestDto request) {
-
-		User user = userRepository.findByUsername(request.username())
-				.orElseThrow(() -> new RuntimeException("Invalid credentials"));
-
-		if (!passwordEncoder.matches(request.password(), user.getPassword())) {
-			return ResponseEntity.status(401).body("Invalid credentials");
-		}
-
-		String token = jwtService.generateToken(user.getId(), user.getRole().toString());
-
-		return ResponseEntity.ok(new AuthResponseDto(token, user.getRole().toString()));
+		return this.authService.login(request);
 	}
 }
--- server/src/main/java/com/hsgg/security/auth/AuthService.java
+++ server/src/main/java/com/hsgg/security/auth/AuthService.java
@@ -1,8 +1,56 @@
 package com.hsgg.security.auth;
 
+import com.hsgg.security.auth.dtos.AuthResponseDto;
+import com.hsgg.security.auth.dtos.LoginRequestDto;
+import com.hsgg.security.auth.dtos.RegisterRequestDto;
+import com.hsgg.security.jwt.JwtService;
+import com.hsgg.security.user.User;
+import com.hsgg.security.user.UserRepository;
+import com.hsgg.security.user.UserRole;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class AuthService {
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+	private final JwtService jwtService;
 
+	public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService) {
+		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
+		this.jwtService = jwtService;
+	}
+
+	public ResponseEntity<?> login(@RequestBody LoginRequestDto request) {
+		User user = userRepository.findByUsername(request.username())
+				.orElseThrow(() -> new RuntimeException("Invalid credentials"));
+
+		if (!passwordEncoder.matches(request.password(), user.getPassword())) {
+			return ResponseEntity.status(401).body("Invalid credentials");
+		}
+
+		String token = jwtService.generateToken(user.getId(), user.getRole().toString());
+		return ResponseEntity.ok(new AuthResponseDto(token, user.getRole().toString()));
+	}
+
+	public ResponseEntity<?> register(@RequestBody RegisterRequestDto request) {
+		if (userRepository.existsByUsername(request.username())) {
+			return ResponseEntity.badRequest().body("Username already taken.");
+		}
+		userRepository.save(toStudent(request));
+		return ResponseEntity.ok("Registered successfully");
+	}
+
+	private User toStudent(RegisterRequestDto request) {
+		return User.builder()
+				.username(request.username())
+				.password(passwordEncoder.encode(request.password()))
+				.firstName(request.firstname())
+				.lastName(request.lastname())
+				.role(UserRole.STUDENT)
+				.build();
+	}
 }
