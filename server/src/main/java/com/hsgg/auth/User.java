e06fd226a59c7e339bc600df1d278946e5608c7d
Yassine Zakhama <yassine.zakhama@alumni.fh-aachen.de>
2025-11-18 13:36:27 +0100

    make user extend UserDetails and Principal

--- server/src/main/java/com/hsgg/auth/User.java
+++ server/src/main/java/com/hsgg/auth/User.java
@@ -4,15 +4,22 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.security.Principal;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
 @NoArgsConstructor
-public class User {
+@EntityListeners(AuditingEntityListener.class)
+public class User implements UserDetails, Principal {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,7 +29,7 @@ public class User {
 	private String username;
 
 	@Column(name = "password_hash", nullable = false)
-	private String passwordHash;
+	private String password;
 
 	@Enumerated(EnumType.STRING)
 	@Column(nullable = false)
@@ -34,5 +41,15 @@ public class User {
 	@PrePersist
 	protected void onCreate() {
 		this.createdAt = LocalDateTime.now();
+	}
+
+	@Override
+	public String getName() {
+		return this.username;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of();
 	}
 }
