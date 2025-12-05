3a75db4f65fa85e61bddbc514ac12430a8bcefa2
Yassine Zakhama <yassine.zakhama@alumni.fh-aachen.de>
2025-12-05 22:38:47 +0100

    remove unnecessary property

--- server/src/main/java/com/hsgg/features/contentBlock/dtos/textBlock/UpdateTextBlockRequest.java
+++ server/src/main/java/com/hsgg/features/contentBlock/dtos/textBlock/UpdateTextBlockRequest.java
@@ -3,7 +3,6 @@ package com.hsgg.features.contentBlock.dtos.textBlock;
 import java.util.Optional;
 
 public record UpdateTextBlockRequest(
-		Long topicId,
 		Optional<String> newTitle,
 		Optional<Integer> newPosition,
 		Optional<String> newText
