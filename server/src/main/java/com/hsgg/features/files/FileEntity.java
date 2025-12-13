79758fdee395f8d55f28fe566ffcc8b65760c0c9
Yassine Zakhama <yzakhama@gmail.com>
2025-12-13 12:18:06 +0100

    enable auto auditing

--- server/src/main/java/com/hsgg/features/files/FileEntity.java
+++ server/src/main/java/com/hsgg/features/files/FileEntity.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -15,6 +16,7 @@ import java.time.LocalDateTime;
 @Setter
 @Getter
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class FileEntity {
 
 	@Id
